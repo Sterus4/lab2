c47880225dd147ae0ad48c2f58ace7c6f55226fc
Sterus4 <368581@niuitmo.ru>
2024-12-03 21:45:30 +0300

    Сделал, вроде работает

--- lab2.srcs/sim_1/new/SortingBuffer.sv
+++ lab2.srcs/sim_1/new/SortingBuffer.sv
@@ -37,21 +37,22 @@ module SortingBuffer(
 
     output reg ready_data,
     output reg [7:0] data_out,
-    output reg [3:0] free_space
+    output reg [4:0] free_space
     );
     reg [7:0] mem [19:0]; // Основная памать
     wire [7:0] outputs[19:0];
-    reg [3:0] current_count;
-    wire [1:0] count_of_queries;
+
+    reg [4:0] current_count;
+    wire [2:0] count_of_queries;
     Sorting20Full mainSorting(.inputs(mem), .outputs(outputs));
     integer i;
     integer j;
 
     assign count_of_queries = valid_write1 + valid_write2 + valid_write3 + valid_write4;
+    assign free_space = 16 - current_count;
 
     initial begin
         current_count = 0;
-        free_space = 16;
         ready_data = 0;
         for(i = 0; i < 20; i = i + 1) begin
             mem[i] = -1;
@@ -59,39 +60,51 @@ module SortingBuffer(
     end
 
     always @(posedge clk or posedge rst) begin
+
         if (rst) begin 
             current_count <= 0;
-            free_space <= 16;
             ready_data <= 0;
             for(j = 0; j < 20; j = j + 1) begin
                 mem[j] = -1;
             end
-        end else begin    
-            if (valid_write1) begin
-                mem[19] <= value1;
-            end else begin
+        end else begin
+            if (count_of_queries <= free_space) begin
+                // Не учитывает считывание
+                if(!valid_read || addr >= current_count) begin 
+                    current_count <= current_count + count_of_queries;
+                end
+                if (valid_write1) begin
+                    mem[19] <= value1;
+                end else begin
+                    mem[19] <= -1;
+                end
+                // Выставляем все значения на свои места (пока без проверки переполнения)
+                if (valid_write2) begin
+                    mem[18] <= value2;
+                end else begin
+                    mem[18] <= -1;
+                end
+                
+                if (valid_write3) begin
+                    mem[17] <= value3;
+                end else begin
+                    mem[17] <= -1;
+                end
+                
+                if (valid_write4) begin
+                    mem[16] <= value4;
+                end else begin
+                    mem[16] <= -1;
+                end
+            end else begin 
                 mem[19] <= -1;
-            end
-            // Выставляем все значения на свои места (пока без проверки переполнения)
-            if (valid_write2) begin
-                mem[18] <= value2;
-            end else begin
                 mem[18] <= -1;
-            end
-            
-            if (valid_write3) begin
-                mem[17] <= value3;
-            end else begin
                 mem[17] <= -1;
-            end
-            
-            if (valid_write4) begin
-                mem[16] <= value4;
-            end else begin
                 mem[16] <= -1;
             end
             // Проверки на считывание
-            if (valid_read) begin
+            if (valid_read && addr < current_count) begin
+                // Действительно считываем значение (оно есть и valid_read установлен)
                 for(i = 0; i < 16; i = i + 1) begin
                     if (i == addr)begin
                         mem[i] <= -1;
@@ -99,9 +112,15 @@ module SortingBuffer(
                         mem[i] <= outputs[i];
                     end
                 end
+                if(count_of_queries <= free_space) begin
+                    current_count <= current_count + count_of_queries - 1;    
+                end else begin
+                    current_count <= current_count - 1;
+                end
                 data_out <= outputs[addr];
                 ready_data <= 1;
             end else begin
+                ready_data <= 0;
                 mem[15:0] <= outputs[15:0];
             end
         end
